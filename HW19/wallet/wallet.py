bb26e0c0b05bdabf31047bd4555f771c179a5205
Emmanuel L Constant <constantemmanuel78@gmail.com>
2021-10-12 16:24:02 -0400

    HW19

--- HW19/wallet/wallet.py
+++ HW19/wallet/wallet.py
@@ -2,7 +2,7 @@
 import subprocess
 import json
 from dotenv import load_dotenv
-from constants import *
+from constants import BTC, BTCTEST, ETH
 import os
 
 
@@ -18,8 +18,13 @@ mnemonic=os.getenv("MNEMONIC")
 from bit import Key, PrivateKey, PrivateKeyTestnet
 from bit.network import NetworkAPI
 from bit import *
-from web3 import Web3
+from web3 import Web3, middleware
+from web3 import Web3, middleware, Account
+from web3.gas_strategies.time_based import medium_gas_price_strategy
+from web3.middleware import geth_poa_middleware
 from eth_account import Account
+
+
 
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
  
@@ -36,54 +41,58 @@ def derive_wallets(mnemonicPhrase, coin, numdrive=3):
 
 coins = {
     BTCTEST: derive_wallets(mnemonic, BTCTEST),
-    ETH: derive_wallets(mnemonic, ETH),
+    ETH: derive_wallets(mnemonic, ETH)
    # BTC: derive_wallets(mnemonic, BTC)
-    }
-print(json.dumps(coins, indent=4, sort_keys=True))
+   }
+#print(json.dumps(coins, indent=4, sort_keys=True))
 
-# Create a function called `priv_key_to_account` that converts privkey strings to account objects.
-def priv_key_to_account(coin):
+btctest_priv_key = coins[BTCTEST][0]['privkey']
+eth_priv_key = coins[ETH][0]['privkey']
+
+## Create a function called `priv_key_to_account` that converts privkey strings to account objects.
+def priv_key_to_account(coin, priv_key):
     if coin == ETH:
-        priv_key = coins[ETH][0]['privkey']
         return Account.privateKeyToAccount(priv_key)
     #elif coin == BTC:
     #    priv_key = coins[BTC][0]['privkey']
     #    return PrivateKey(priv_key)
     if coin == BTCTEST:
-        priv_key = coins[BTCTEST][0]['privkey']
-        return PrivateKeyTestnet(priv_key)
+       return PrivateKeyTestnet(priv_key)
 
 #accBTC = priv_key_to_account(BTC)
-accETH = priv_key_to_account(ETH)   
-accBTCTEST = priv_key_to_account(BTCTEST)
+accETH = priv_key_to_account(ETH, eth_priv_key)   
+accBTCTEST = priv_key_to_account(BTCTEST, btctest_priv_key)
+#print(accBTCTEST)
 
-# Create a function called `create_tx` that creates an unsigned transaction appropriate metadata.
+
+## Create a function called `create_tx` that creates an unsigned transaction appropriate metadata.
 def create_tx(coin, account, to, amount):
-    if coin == ETH:
-        gas_estimate = w3.eth.estimate_gas(
-            {"from": account.address, "to": to, "value": amount}
-        )
-        return {
-            'from': account.address,
-            "to": to,
-            "value": amount,
-            "gasPrice": w3.eth.gasPrice,
-            "gas": gas_estimate,
-            "nonce": w3.eth.getTransactionCount(account.address),
-            "chainId": w3.eth.chain_id
-        }
-    #elif coin == BTC:
-    #    return PrivateKey.prepare_transaction(account.address, [(to,amount,BTC)])
-    if coin == BTCTEST:
-        return PrivateKeyTestnet.prepare_transaction(account.address, [(to,amount,BTCTEST)])
-
-
+   if coin == ETH:
+       gas_estimate = w3.eth.estimate_gas(
+           {"from": account.address, "to": to, "value": amount}
+       )
+       return {
+           'from': account.address,
+           "to": to,
+           "value": amount,
+           "gasPrice": w3.eth.gasPrice,
+           "gas": gas_estimate,
+           "nonce": w3.eth.getTransactionCount(account.address),
+           "chainId": w3.eth.chain_id
+       }
+   #elif coin == BTC:
+   #    return PrivateKey.prepare_transaction(account.address, [(to,amount,coin)])
+   if coin == BTCTEST:
+       return PrivateKeyTestnet.prepare_transaction(account.address, [(to,amount,BTC)])
+print(accBTCTEST.address)
+print(accETH.address)
+#tx_test = create_tx(BTCTEST, accBTCTEST,'2N83F3qNp5A13EtUxhUpHLzYGMZZqGkDwtS', 0.000000001)
 
 
 
 # Create a function called `send_tx` that calls `create_tx`, signs and sends the transaction.
-def send_tx(coin, account,recipent, amount):
-    tx = create_tx(coin, account,recipent, amount)
+def send_tx(coin, account, recipent, amount):
+    tx = create_tx(coin, account, recipent, amount)
     signed = account.sign_transaction(tx)
     if coin == ETH:
         return w3.eth.sendRawTransaction(signed.rawTransaction)
@@ -94,5 +103,6 @@ def send_tx(coin, account,recipent, amount):
 
 #send_tx(ETH, accETH, '0xa87601B8dbA7C17081E67eC68E9aB749Fd7e1d07', 1)
 #send_tx(BTC, accBTC, '', 0.0000001)
-send_tx(BTCTEST, accBTCTEST, '37NFX8KWAQbaodUG6pE1hNUH1dXgkpzbyZ', 0.000000001)
+
+send_tx(BTCTEST, accBTCTEST, '2N83F3qNp5A13EtUxhUpHLzYGMZZqGkDwtS', 0.000000001)
 print(priv_key_to_account(BTCTEST, (coins[BTCTEST][0]['privkey'])).get_transactions()[0])
